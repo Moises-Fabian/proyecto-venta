958b90b5537374ec219b50e46b84c39422d53c89
Moises <m.lagos1995@gmail.com>
2021-10-15 20:41:01 -0300

    maqueta menu material

--- Frontend-Venta/src/app/app-routing.module.ts
+++ Frontend-Venta/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { isEmptyExpression } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'cliente', component: ClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
